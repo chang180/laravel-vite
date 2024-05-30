f810e2a2142cf67d4c7e4bfed85aa774349deb96
張建文 <chang180@gmail.com>
2024-05-30 16:38:31 +0800

    Episode 2 finished.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,29 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel-vite</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
+    <title>Laravel-vite</title>
 
-    </body>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+
+<body class="bg-slate-900 text-slate-400">
+    <div class="text-center pt-10">
+        <h1 class="text-3xl font-extrabold text-slate-200">
+            Hi
+        </h1>
+        <h4 class="text-xl pt-3">
+            Welcome to Laravel-vite
+        </h4>
+    </div>
+    <div class="flex justify-center items-center pt-12">
+        <img
+        class="object-fill h-96"
+        src="{{ Vite::image('logo.jpg') }}" alt="">
+    </div>
+</body>
+
 </html>
